ec7d9ef09c6516c543b932e51f4508385c4dfba1
nouseforaname2 <104241872+nouseforaname2@users.noreply.github.com>
2023-12-15 20:47:08 -0800

    Final Submission

--- backend/middleware/request-time.js
+++
@@ -1,8 +0,0 @@
-const { response } = require("express");
-
-const requestTime = (request, response, next) => {
-  console.log(`A request was made at ${Date.now()}: ${request.method}`);
-  next();
-};
-
-module.exports = requestTime;
